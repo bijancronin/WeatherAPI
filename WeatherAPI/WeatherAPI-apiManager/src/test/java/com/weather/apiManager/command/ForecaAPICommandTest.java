c4c9e414e1a3c3a01f19d839e44a0deaa8ae5714
mohebbihr <mohebbi.h@gmaill.com>
2017-04-26 22:30:00 -0400

    Add files via upload

---
+++ WeatherAPI/WeatherAPI-apiManager/src/test/java/com/weather/apiManager/command/ForecaAPICommandTest.java
@@ -0,0 +1,54 @@
+package com.weather.apiManager.command;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+public class ForecaAPICommandTest {
+	
+    private WeatherAPIKey key1, key2;
+    private WeatherAPIGeoLocation location1, location2;
+    private WeatherAPICommand api1, api2;
+    
+    @Before
+    public void setUp() {
+        key1 = new WeatherAPIKey("KP1vQAGGx2Qx8adVWO83dqkvoqg");
+        location1 = new WeatherAPIGeoLocation();
+        location1.setLat(37.8267);
+        location1.setLongit(-122.4233);
+        location1.setCity("");
+        location1.setState("");
+        location1.setCountry("");
+        api1 = new ForecaAPICommand(key1, location1);
+        key2 = new WeatherAPIKey("invalid");
+        location2 = new WeatherAPIGeoLocation();
+        location2.setLat(42.3601);
+        location2.setLongit(71.0589);
+        location2.setCity("");
+        location2.setState("");
+        location2.setCountry("");
+        api2 = new ForecaAPICommand(key2, location2);
+    }
+    
+    @Test
+    public void testExecuteSuccess() {
+        String response = api1.execute();
+        assertThat(response, is(notNullValue()));
+        
+        // Need to check if response is coming from DB.
+        // Since in previous request the response must have
+        // been saved to DB.
+        String responseFromDB = api1.execute();
+        assertThat(response, is(responseFromDB));
+    }
+    
+    @Test
+    public void testExecuteFailure() {
+        String response = api2.execute();
+        assertThat(response, is(nullValue()));
+    }
+    
+}
